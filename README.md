395b283ba079ce733267b98dc6aad18e51c546c9
Yauhen Artsiukhou <jsirex@gmail.com>
2024-07-12 15:21:19 +0200

    Update README and installer

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ guarantees provided.
 Installer expects you install minimal **GNU Debian Linux** version
 **using netinst cd image**.
 
-Use, for example the following link:
+Use, for example, the following link:
 https://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/debian-testing-amd64-netinst.iso
 
 Or you can take a full CD/DVD installation. It depends on required
@@ -70,23 +70,31 @@ from backups.
 
 The following layout is recommended:
 
-- UEFI: 200 MB, Boot flag on
-- Root: 35 GB, Ext4, /, SET PARTLABEL=root
+- Force UEFI Installation: **YES**
+- Partition method: manual. Create new partition table. GPT is preferred.
+- #1 UEFI: 200 MB, Boot flag on
+- #2 Root: 35 GB, Ext4, /, SET PARTLABEL=root
   - You can make root partition less or more depending on your drive capacity
-  - OS and ITGMania takes less than 1 GB
-  - Log files, ITGMania save files can take some space
+  - OS and **ITGMania** takes less than 1 GB
+  - Log files, **ITGMania** save files can take some space
   - Additional packages, drivers can take some space too
   - For experiments, installing SDK, building **ITGMania** from scratch more space is needed
   - 20-35 GB is enough for experiments, install additional games
-- Backup: 35 GB, do nothing, SET PARTLABEL=backup
+- #3 Backup: 35 GB, do nothing, SET PARTLABEL=backup
   - Same size as Root partition
   - Can be smaller until total used space by `root` fits into partition
   - However, not recommended to make it smaller
-- Songs: 100 GB+, Ext4, do not mount partition, SET PARTLABEL=songs
+- #4 Songs: 100 GB+, Ext4, do not mount partition, SET PARTLABEL=songs
   - It depends on your songs' collection
 - Any other partitions you need
 
 For running **ITGMania** with enough RAM swap is not required.
+
+For example at the end it should like this (I used VirtualBox, but
+your partitions should have different size).
+
+![disk-partition.png](images/disk-partition.png)
+
 
 ## Reboot
 
@@ -94,9 +102,7 @@ Reboot to your fresh installed system, install required firmware, etc.
 
 # ITG Machine Installer
 
-Copy this project onto target machine and run `./install.sh`.
-Curently only `install.sh` is used, so you can do **curl+bash**
-magic. At this point it is expected that you have a mininmal Debian
+At this point it is expected that you have a minimal Debian
 system installed and running.
 
 **Internet access** must be provided. Your options:
@@ -107,5 +113,38 @@ system installed and running.
   least it works for Android
 - You have configured Internet access somehow
 
-*HINT: If you connected with Android or cabel just type `dhclient` to
+*HINT: If you connected with Android or cable just type `dhclient` to
 setup network. It requires only before you get your network manager.*
+
+You need to run `instal.sh` script either by copying it or by:
+
+`wget -O- https://raw.githubusercontent.com/jsirex/itgmachine/master/install.sh | bash -s`
+
+
+# After install
+
+If everything went fine you probably should see the following output of `df -h`:
+
+``` bash
+Filesystem      Size  Used Avail Use% Mounted on
+udev            6.6G     0  6.6G   0% /dev
+tmpfs           1.4G  1.8M  1.4G   1% /run
+/dev/nvme0n1p2   32G  2.2G   30G   7% /
+tmpfs           6.6G     0  6.6G   0% /dev/shm
+efivarfs        192K  111K   77K  60% /sys/firmware/efi/efivars
+tmpfs           5.0M     0  5.0M   0% /run/lock
+tmpfs           6.6G     0  6.6G   0% /tmp
+tmpfs           1.0M     0  1.0M   0% /run/credentials/systemd-udev-load-credentials.service
+tmpfs           1.0M     0  1.0M   0% /run/credentials/systemd-journald.service
+tmpfs           1.0M     0  1.0M   0% /run/credentials/systemd-tmpfiles-setup-dev-early.service
+tmpfs           1.0M     0  1.0M   0% /run/credentials/systemd-sysctl.service
+tmpfs           1.0M     0  1.0M   0% /run/credentials/systemd-tmpfiles-setup-dev.service
+/dev/nvme0n1p4  155G  110M  155G   1% /home/itg/.itgmania/Songs
+/dev/nvme0n1p1  188M   54M  134M  29% /boot/efi
+tmpfs           1.0M     0  1.0M   0% /run/credentials/systemd-tmpfiles-setup.service
+tmpfs           1.0M     0  1.0M   0% /run/credentials/getty@tty3.service
+tmpfs           1.4G   12K  1.4G   1% /run/user/0
+tmpfs           1.4G   16K  1.4G   1% /run/user/1000
+```
+
+Notice, that OS required only 2.2G
--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 # -*- mode: bash-ts -*-
 
-# shellcheck disable=SC2164
-# ROOT_DIR=$(cd "$(dirname "$0")"; pwd)
+# Default screen title can be overwritten in screen menu
+screen_title="ITG Machine"
 
-ITGMACHINE_CACHE=/var/cache/itgmachine
-# ITGMACHINE_INSTALL=/usr/local/games
-
+### Whiptail interface
+# Each function re-sets wt_status and wt_out
 msgbox() {
     local message="$1";
+    local height=10
+    local lines=0
+
+    lines=$(echo "$message" | wc -l)
+    [[ $lines -gt 4 ]] && height=25
 
     whiptail --title "$screen_title" \
-             --msgbox "$message" 8 80
+	     --backtitle "Use <up>/<down> to navigate, <enter> to select, <tab> to switch between buttons." \
+	     --scrolltext \
+             --msgbox "$message" $height 80
 
     wt_status=$?
     unset wt_out
@@ -21,17 +27,21 @@ yesnobox() {
     local message="$1"
 
     whiptail --title "$screen_title" \
-             --yesno "$message" 8 80
+	     --backtitle "Use <up>/<down> to navigate, <enter> to select, <tab> to switch between buttons." \
+             --yesno "$message" 10 80
 
     wt_status=$?
     unset wt_out
+
+    return $wt_status
 }
 
 inputbox() {
     local message="$1"
     local default="$2"
     wt_out=$(whiptail --title "$screen_title" \
-		      --inputbox "$message" 8 80 \
+		      --backtitle "Use <up>/<down> to navigate, <enter> to select, <tab> to switch between buttons." \
+		      --inputbox "$message" 10 80 \
                       "$default" 3>&1 1>&2 2>&3)
     wt_status=$?
 }
@@ -40,7 +50,8 @@ passbox() {
     local message="$1"
     local default="$2"
     wt_out=$(whiptail --title "$screen_title" \
-		      --passwordbox "$message" 8 80 \
+		      --backtitle "Use <up>/<down> to navigate, <enter> to select, <tab> to switch between buttons." \
+		      --passwordbox "$message" 10 80 \
                       "$default" 3>&1 1>&2 2>&3)
     wt_status=$?
 }
@@ -49,6 +60,7 @@ textbox() {
     local file="$1";
 
     whiptail --title "$screen_title" \
+	     --backtitle "Use <up>/<down> to navigate, <enter> to select, <tab> to switch between buttons." \
              --scrolltext \
              --textbox "$file" 25 80
 
@@ -56,23 +68,85 @@ textbox() {
     unset wt_out
 }
 
+radiobox() {
+    local text="Choose item from the list below:"
+    if [[ -n "$1" ]]; then text="$1"; fi; shift
+
+    local items=("$@")
+
+    while :; do
+	wt_out=$(whiptail --title "$screen_title" \
+			  --backtitle "Use <up>/<down> to navigate, <space> to select, <tab> to switch between buttons." \
+			  --ok-button "Select" \
+			  --cancel-button "Back" \
+			  --notags \
+			  --radiolist "$text" \
+                          25 80 16 \
+                          "$@" 3>&1 1>&2 2>&3)
+	wt_status=$?
+
+	if [[ $wt_status -ne 0 ]]; then break; fi
+	if [[ -n "$wt_out" ]]; then break; fi
+	msgbox "Nothing was selected. HINT: Use space to select an item."
+    done
+
+    return "$wt_status"
+}
+
 menubox() {
+    # screen title can be changed in submenu
+    # this way we recursively persist it for the selected menu
     local title="$screen_title"
+    local selected="none"
+    local text="Select an action:"
+
+    if [[ -n "$1" ]]; then text="$1"; fi; shift
+
+    local items=("$@")
+
     while :; do
 	wt_out=$(whiptail --title "$title" \
+			  --backtitle "Use <up>/<down> to navigate, <enter> to select, <tab> to switch between buttons." \
 			  --ok-button "Select" \
-			   --cancel-button "Exit" \
-			   --notags \
-			   --menu "" \
-                           25 80 16 \
-                           "$@" 3>&1 1>&2 2>&3)
+			  --cancel-button "Back" \
+			  --notags \
+			  --default-item "$selected" \
+			  --menu "$text" \
+                          25 80 16 \
+                          "${items[@]}" 3>&1 1>&2 2>&3)
 	wt_status=$?
+	selected="$wt_out"
 
 	if [[ $wt_status -ne 0 ]]; then break; fi
 	if [[ "$wt_out" == screen_* ]]; then $wt_out; else break; fi
     done
 }
 
+# Runs command and delays so you can see result
+run() {
+    local status
+    # Run command
+    "$@"
+    status=$?
+
+    [[ $status -eq 0 ]] || (echo "Exist status $status. Press enter to continue"; read -r)
+
+    return $status
+}
+
+
+fstab_flash() {
+    cat << EOF > /tmp/fstab
+/dev/disk/by-path/pci-0000:00:13.2-usb-0:6:1.0-scsi-0:0:0:0-part1       /mnt/P1 auto    rw,noatime,noauto,user  0       0
+/dev/disk/by-path/pci-0000:00:13.2-usb-0:6:1.0-scsi-0:0:0:0             /mnt/P1 auto    rw,noatime,noauto,user  0       0
+
+/dev/disk/by-path/pci-0000:00:13.2-usb-0:5:1.0-scsi-0:0:0:0-part1       /mnt/P2 auto    rw,noatime,noauto,user  0       0
+/dev/disk/by-path/pci-0000:00:13.2-usb-0:5:1.0-scsi-0:0:0:0             /mnt/P2 auto    rw,noatime,noauto,user  0       0
+EOF
+
+}
+
+### Ensure functions
 ensure_root() {
     if [[ $UID -ne 0 ]]; then
         msgbox "You must run this program as root"
@@ -82,7 +156,7 @@ ensure_root() {
 
 ensure_apt_update() {
     if [[ -z "$ITGMACHINE_APT_UPDATED" ]]; then
-	apt update
+	run apt -o APT::Update::Error-Mode=any update
 	ITGMACHINE_APT_UPDATED=true
     fi
 }
@@ -92,7 +166,7 @@ ensure_apt_package() {
 
     apt install "$@"
     apt_status=$?
-    [[ $apt_status -eq 0 ]] || (echo "Exist status $apt_status. Press enter to continue"; read dummy)
+    [[ $apt_status -eq 0 ]] || (echo "Exist status $apt_status. Press enter to continue"; read -r)
 }
 
 ensure_command() {
@@ -101,10 +175,8 @@ ensure_command() {
 
     command -v "$cmd" &> /dev/null && return
 
-    yesnobox "Command $cmd was not found.\nInstall $package?"
-    if [[ $wt_status -eq 0 ]]; then
-	ensure_apt_package "$package"
-    fi
+    yesnobox "Command $cmd was not found.\nInstall $package?" \
+	&& ensure_apt_package "$package"
 }
 
 ensure_itgmachine_cache() {
@@ -132,11 +204,11 @@ install_itgmania() {
 }
 
 screen_apt_repository() {
+    screen_title="ITG Machine - APT"
+
     local url="https://ftp.debian.org/debian"
     local release="testing"
     local components="main contrib non-free non-free-firmware"
-
-    screen_title="ITG Machine - APT"
 
     inputbox "Enter Debian repository URL:" "$url"
     if [[ $wt_status -ne 0 ]]; then return; fi
@@ -150,7 +222,7 @@ screen_apt_repository() {
     if [[ $wt_status -ne 0 ]]; then return; fi
     components="$wt_out"
 
-    yesnobox "Replace source.list with:\ndeb $url $release $components?"
+    yesnobox "Replace source.list with:\nURL: $url\nRelease: $release\nComponents: $components?"
     if [[ $wt_status -eq 0 ]]; then
 	echo "deb $url $release $components" > /etc/apt/sources.list
 	screen_apt_update 1
@@ -170,7 +242,8 @@ screen_apt_update() {
 screen_apt_upgrade() {
     screen_apt_update
 
-    apt dist-upgrade
+    screen_title="ITG Machine - Upgrading"
+    run apt dist-upgrade
 }
 
 
@@ -214,29 +287,85 @@ screen_mount_songs() {
     chown itg:itg /home/itg/.itgmania/Songs/.
 }
 
-screen_disk() {
-    whiptail --title "ITG Machine - WARN" \
-	     --ok-button "Continue" \
-	     --msgbox "Consult with README before proceed" 8 80
+screen_disk_primary() {
+    screen_title="ITG Machine - Disk"
 
-    while :; do
-	_choice=$(whiptail --title "ITG Machine - Disk" \
-			   --ok-button "Select" \
-			   --cancel-button "Exit" \
-			   --notags \
-			   --menu "" 25 80 17 \
-			   "screen_mount_songs" "Make Songs mount point" \
-			   3>&1 1>&2 2>&3)
-	_status=$?
-	if [[ $_status -ne 0 ]]; then break; fi
-	if [[ "$_choice" == screen_* ]]; then $_choice; else break; fi
-    done
+    local disks
+    local radioitems=()
+
+    disks=$(lsblk -dnpP -o NAME,MODEL)
+
+    while read -r line; do
+	eval "$line"
+	radioitems+=("$NAME" "$NAME ($MODEL)" off)
+    done <<< "$disks"
+    unset NAME
+    unset MODEL
+
+    radiobox "Select primary drive where system is installed:" "${radioitems[@]}" || return
+    primary_disk="$wt_out"
+    msgbox "You have selected $primary_disk as primary disk."
 }
 
-screen_networkmanager() {
+screen_disk_partitions() {
+    local radioitems=()
+    local partitions
+    local pttype
+
+    if [[ -z "$primary_disk" ]]; then
+	msgbox "Primary disk is not selected. It should look like /dev/sda or /dev/nvme0n1."
+	return 1
+    fi
+    pttype=$(lsblk -dn -o PTTYPE "$primary_disk")
+    if [[ ! "$pttype" == "gptx" ]]; then
+	msgbox "The parition type is not GPT ($pttype). Installer
+can't use PARTLABEL to detect and mount root, backup and songs
+partitions.
+
+WARNING! You can't use filesystem's LABEL or UUID because
+filesystem's metadata will be copied with filesystem backup or
+restore procedure.
+
+Installer will offer you to use partition device directly. Sometimes
+it is reliable if you insert additional disks."
+    fi
+
+    return
+
+    partitions=$(lsblk -npP -o NAME,PARTLABEL,LABEL,TYPE)
+
+    while read -r line; do
+	eval "$line"
+
+	radioitems+=("$NAME" "$NAME ($MODEL)" off)
+    done <<< "$partitions"
+    unset NAME
+    unset PARTLABEL
+    unset LABEL
+    unset TYPE
+
+    radiobox "Select primary drive where system is installed:" "${radioitems[@]}" || return
+    primary_disk="$wt_out"
+    msgbox "You have selected $primary_disk as primary disk."
+
+
+    root_dev=$(blkid -t PARTLABEL=root -o device "$primary"*)
+    backup_dev=$(blkid -t PARTLABEL=backup -o device "$primary"*)
+    songs_dev=$(blkid -t PARTLABEL=songs -o device "$primary"*)
+
+    echo $root_dev
+    echo $backup_dev
+    echo $songs_dev
+    run /bin/false
+
+
+}
+
+screen_network_manager() {
+    screen_title="ITG Machine - Network Manager"
     screen_apt_update
 
-    apt install network-manager
+    run apt install network-manager
 }
 
 screen_uefi_kernel() {
@@ -260,7 +389,7 @@ screen_uefi_kernel() {
 	       -u 'root=PARTLABEL=root itgmachine.mode=backup rw quiet nmi_watchdog=0 initrd=\EFI\itgmachine\initrd.img'
 }
 
-screen_wifi() {
+screen_network_wifi() {
     screen_title="ITG Machine - WIFI"
     ensure_command nmcli network-manager
 
@@ -274,7 +403,7 @@ screen_wifi() {
         menuitems+=("$ssid ($rate $bars)")
     done <<< "$wifi_networks"
 
-    menubox "${menuitems[@]}"
+    menubox "" "${menuitems[@]}"
 }
 
 screen_wifi_connect() {
@@ -286,7 +415,7 @@ screen_wifi_connect() {
 
     nmcli device wifi connect "$ssid" password "$wt_out"
     nmcli_status=$?
-    [[ $nmcli_status -eq 0 ]] || (echo "Exist status $nmcli_status. Press enter to continue"; read dummy)
+    [[ $nmcli_status -eq 0 ]] || (echo "Exist status $nmcli_status. Press enter to continue"; read -r)
 }
 
 screen_sound_pipewire() {
@@ -320,36 +449,98 @@ EOF
 
 }
 
-screen_system() {
-    screen_title="ITG Machine - System"
-    menubox screen_apt_repository "Configure Debian Testing repository" \
-            screen_apt_upgrade "Perform Debian Upgrade" \
-	    screen_uefi_kernel "Install kernel to UEFI partition" \
-            screen_install_networkmanager "Install Network Manager" \
-            screen_wifi "Configure WiFi Network" \
-            screen_sound_pipewire "Setup Pipewire" \
-	    screen_sddm "Configure SDDM to run ITGMania"
+screen_pacdrive() {
+    cat << EOF > /etc/udev/rules.d/75-linux-pacdrive.rules
+SUBSYSTEM=="usb", ATTRS{idVendor}=="d209", ATTRS{idProduct}=="150[0-9]", MODE="0666"
+EOF
+}
 
+screen_network_dhclient() {
+    screen_title="ITG Machine - DHCLIENT"
+
+    msgbox "You current network configuration is displayed below.
+If you pluged in any wired connection try dhclient to set it up.
+If you already have Internet connection, say no.
+
+    $(ip addr)"
+
+    yesnobox "Try dhclient?" && run dhclient
+}
+
+screen_first_reboot() {
+    msgbox "After initial installation and upgrade new critical
+packages may be upgraded. If network manager is installed it is
+also good time to check weather everything is working.
+Reboot machine and continue system setup from where you where."
+
+    yesnobox "Reboot?" && run reboot
+}
+
+screen_system_first() {
+    screen_title="ITG Machine - System"
+
+    menubox "" \
+	    screen_network_dhclient "Ad-hoc connect to the network " \
+	    screen_apt_repository "Setup Debian Repository" \
+            screen_apt_upgrade "Upgrade Debian" \
+            screen_network_manager "Setup Network Manager" \
+            screen_network_wifi "Setup WiFi Network (optional)" \
+	    screen_first_reboot "Reboot after initial setup"
+}
+
+
+screen_system_second() {
+    screen_title="ITG Machine - System (Continue)"
+
+    menubox "" \
+	    screen_disk_primary "Select primary disk" \
+	    screen_disk_partitions "Detect ITG Machine partitions" \
+	    screen_pacdrive "Setup Linux PacDrive" \
+	    screen_uefi_kernel "Install kernel to UEFI partition" \
+            screen_sound_pipewire "Setup Pipewire" \
+	    screen_grub "Configure GRUB" \
+	    screen_openssh "Install openssh server" \
+	    screen_efi "Configure EFI" \
+	    screen_sddm "Configure SDDM to run ITGMania"
 }
 
 screen_itgmania() {
     screen_title="ITG Machine - ITG Mania"
-    menubox "screen_itgmania_install 0.9.0" "Install ITGmania 0.9.0" \
+
+    menubox "" \
+	    "screen_itgmania_install 0.9.0" "Install ITGmania 0.9.0" \
 	    "screen_itgmania_install 0.8.0" "Install ITGmania 0.8.0" \
-	    "screen_itgmania_install" "Install ITGmania other version"
-}
-
-screen_main() {
-    screen_title="ITG Machine"
-    [[ $ITGDEBUG == "yes" ]] || ensure_root
-
-    menubox screen_system "Manage System" \
-	    screen_disk "Configure disk" \
-	    screen_itgmania "ITG Mania" \
-	    screen_grub "Configure GRUB" \
-	    screen_efi "Configure EFI" \
-	    screen_upgrades "Configure unattended upgrades" \
+	    "screen_itgmania_install" "Install ITGmania other version" \
 	    screen_boogiestats "Configure Boogie Stats"
 }
 
+screen_troubleshoot() {
+    screen_title="ITG Machine - Troubleshoot"
+
+    menubox "" \
+	    screen_check_disk_layout "Check disk layout"
+}
+
+screen_main() {
+    screen_title="ITG Machine - Main"
+
+    menubox "" \
+	    screen_system_first "Setup System (first boot)" \
+	    screen_system_second "Setup System (second boot)" \
+	    screen_itgmania "Setup ITG Mania" \
+	    screen_troubleshoot "Troubleshoot ITG Machine"
+}
+
+# TUI is based on whiptail. We must check it manually before anything else
+if ! command -v whiptail &> /dev/null; then
+    echo "whiptail is not available. Graphic install is not possible!"
+    echo "This should not happen because whiptail has important priority"
+    echo "and installed automatically with any Debian. Not nice"
+
+    # This is not nice: !69=!(0b1000101)=(0b0111010)=58
+    exit 58
+fi
+
+ensure_root
 screen_main
+
